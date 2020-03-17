9403f41848cb65a5b95146479ccf1a4e5a0570a2
rayoungji <dudwl7676@naver.com>
2020-03-17 16:45:37 +0900

    팩토리얼/ 팩토리얼 0의 갯수 찾기

---
+++ src/lecture3/ex_1676.java
@@ -0,0 +1,13 @@
+package lecture3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ex_1676 {
+    public static void main(String arg[]) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+        System.out.println(t/5+t/25+t/125);
+    }
+}

a33e0538cb8310fbe978ed7ef648ad57f61ca766
rayoungji <dudwl7676@naver.com>
2020-04-23 15:02:35 +0900

    가장 긴 증가하는 부분 수열4(LIS 직접 구하기)

---
+++ src/lecture4/ex_14002.java
@@ -0,0 +1,50 @@
+package lecture4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class ex_14002 {
+    public static void main(String arg[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String str[] = br.readLine().split(" ");
+        int arr[] = new int[1001];
+        int dp[] = new int[1001];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(str[i]);
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (dp[i] == 0) {
+                dp[i] = 1;
+            }
+            for (int j = 0; j < i; j++) {
+                if (dp[i] < dp[j] + 1 && arr[i] > arr[j]) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        int value=dp[0];
+        for(int i=1; i<dp.length; i++){
+            value=Math.max(value,dp[i]);
+        }
+
+        System.out.println(value);
+
+        Stack<Integer>stack=new Stack<>();
+        for(int i=n-1; i>=0; i--){
+            if(value==dp[i]){
+                stack.add(arr[i]);
+                value--;
+            }
+        }
+        while(!stack.isEmpty()){
+            System.out.print(stack.pop());
+            System.out.print(" ");
+        }
+    }
+}
